101d31e0c561c5f699c5071d1e1bd47215bc7fcc
Jacklenzireen <jacklenazirien@gmail.com>
2024-05-08 20:50:18 +0200

    first commit

---
+++ List1.py
@@ -0,0 +1,11 @@
+number = [12,75,150,180,145,525,50]
+for i in number:
+    if i>150 :
+        continue
+    elif  i>500:
+        break
+    else :
+        if i%5 == 0 :
+            print(i)
+    
+
